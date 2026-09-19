9370cc2776319121436928bc6c3c2d4f2fb69599
Denis Ethier <decodingtheburn@gmail.com>
2026-09-19 10:48:38 -0400

    auto-backup: pre-ai edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://decodingtheburn.com',
+	devToolbar: {
+		enabled: false,
+	},
 	integrations: [mdx(), sitemap()],
 });
